4391aba1cc26369d5263c767a4ef260192166f18
sawan <sawan.d@mantralabsglobal.com>
2022-01-25 00:24:30 +0530

    half UI is done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BsPlusLg } from "react-icons/bs";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-slate-800 flex items-center justify-center flex-auto">
-      <div className="flex flex-col">
-        <h1 className="text-4xl font-serif animate-bounce text-green-500">
-          Hello ReactJS community, I have joined you. Don't worry.
-        </h1>
-        {/* <button className="bg-blue-600 p-1 text-center w-28 rounded-full text-gray-300">
-          Launch
-        </button> */}
-        <button class="btn btn-primary">daisyUI Button</button>
+    <div className="w-full h-screen bg-teal-900 flex items-center justify-center flex-auto">
+      <div className="flex flex-col w-96 rounded-sm">
+        <div className="px-2 flex items-center text-gray-700 text-base justify-between h-10 bg-teal-400 font-serif rounded-sm">
+          <GiHamburgerMenu className="h-6 w-6"/>
+          <span className="">Enlist your tasks</span>
+          <div></div>
+        </div>
+        <div className="h-80 w-96 mt-1 bg-gray-300 flex items-stretch justify-center rounded-sm">
+          <button class="btn btn-accent mb-1 text-gray-700 w-20 btn-sm self-end rounded-md flex flex-row px-1">
+            <BsPlusLg className="h-3 w-3"/>
+            <span className="ml-3 font-serif">Add</span> 
+          </button>
+        </div>
       </div>
     </div>
   );
